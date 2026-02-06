0039caccbeca8fcde87f1871f6489ec932ffe349
Dhruvin Rupesh Soni <dhruvinrsoni@gmail.com>
2026-02-06 12:36:13 +0530

    chore: Update service worker revision for index.html
    - Updates the service worker's revision for index.html.
    - This ensures that the latest version of the index.html file is cached and served to users.
    
    Signed-off-by: Dhruvin Rupesh Soni <dhruvinrsoni@gmail.com>

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-237f2c1f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.u59g0fqcij"
+    "revision": "0.cfvdc7vd5d"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
